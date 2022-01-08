74e08f9deaebfaa721fd5717bff15e5154b3d83b
Nikit117 <77112359+Nikit117@users.noreply.github.com>
2022-01-09 00:10:57 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask,request, url_for, redirect, render_template
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+model=pickle.load(open('model.pkl','rb'))
+
+
+@app.route('/')
+def hello_world():
+    return render_template("nikit.html")
+
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    int_features=[int(x) for x in request.form.values()]
+    final=[np.array(int_features)]
+    prediction=model.predict(final)
+    return render_template('nikit.html',pred = float(prediction*10000) )
+if __name__ == '__main__':
+    app.run(debug=True)
